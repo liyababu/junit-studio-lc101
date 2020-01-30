38ee4330858ed2c12dabc5a91824252d5bff3847
liyababu <50897099+liyababu@users.noreply.github.com>
2020-01-29 19:57:48 -0600

    Added code

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -2,6 +2,9 @@ package main;
 
 
 public class BalancedBrackets {
+    public BalancedBrackets(String s) {
+    }
+
     /**
      * The function BalancedBrackets should return true if and only if
      * the input string has a set of "balanced" brackets.
@@ -28,6 +31,9 @@ public class BalancedBrackets {
                 brackets++;
             } else if (ch == ']') {
                 brackets--;
+            }
+            if (brackets < 0) {
+                return false;
             }
         }
         return brackets == 0;
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,57 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
-}
+    @Test
+    public void pairedBracketReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launchcode]"));
+    }
+
+    @Test
+    public void pairOfBracketInBetweenTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[code]"));
+    }
+
+    @Test
+    public void pairedBracketAtBeginning(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]Launchcode"));
+    }
+    @Test
+    public void noPairOfBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void onlyOneOpeningBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));;
+    }
+    @Test
+    public void pairOfBracketsInReverseReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));;
+    }
+    @Test
+    public void onlyOneClosingBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode]"));;
+    }
+    @Test
+    public void noClosingBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch[Code"));;
+    }
+    @Test
+    public void noOpeningBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code]"));;
+    }
+    @Test
+    public void pairOfBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launch][Code]"));;
+    }
+
+
+
+
+
+    }
